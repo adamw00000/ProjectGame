0e66b7df22302afbd0165a2c551f0cbe6111441f
Krzysztof Więcław <wutnik@gmail.com>
2019-03-01 14:56:36 +0100

    Initial commit with .Net Core Console App project
    Signed-off-by: Krzysztof Więcław <wutnik@gmail.com>

---
+++ ProjectGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
