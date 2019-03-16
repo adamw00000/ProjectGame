acc058d18d2398bc71bd37669d363c202865f14c
Marcin Zakrzewski <zakrzewskim2@student.mini.pw.edu.pl>
2019-03-16 12:54:05 +0100

    Avalonia project added

---
+++ ProjectGameGUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectGameGUI.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Hello World!";
+    }
+}
