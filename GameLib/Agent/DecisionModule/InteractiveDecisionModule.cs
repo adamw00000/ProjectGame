73ba02e1ab6d10d7ed88b41a846d19657c09f416
Marcin Zakrzewski <zakrzewskim2@student.mini.pw.edu.pl>
2019-03-19 23:17:13 +0100

    Interactive decision module implemented

--- GameLib/Agent/DecisionModule/InteractiveDecisionModule.cs
+++ GameLib/Agent/DecisionModule/InteractiveDecisionModule.cs
@@ -1,15 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using GameLib.Actions;
 
 namespace GameLib
 {
     public class InteractiveDecisionModule : IDecisionModule
     {
-        public IAction ChooseAction(int agentId, AgentState agentState)
+        private bool registered = false;
+
+        public async Task<IAction> ChooseAction(int agentId, AgentState agentState)
         {
-            throw new NotImplementedException();
+            if (!registered)
+            {
+                InteractiveInputProvider.Register(agentId);
+                registered = true;
+            }
+
+            ConsoleKey key = await InteractiveInputProvider.GetKey(agentId);
+
+            IAction action = ParseInput(key, agentId, agentState);
+            Console.WriteLine(action);
+
+            return action;
+        }
+
+        private IAction ParseInput(ConsoleKey key, int agentId, AgentState agentState)
+        {
+            IAction action;
+            var random = RandomGenerator.GetGenerator();
+
+            switch (key)
+            {
+                case ConsoleKey.UpArrow:
+                    action = new ActionMove(agentId, MoveDirection.Up);
+                    break;
+                case ConsoleKey.DownArrow:
+                    action = new ActionMove(agentId, MoveDirection.Down);
+                    break;
+                case ConsoleKey.LeftArrow:
+                    action = new ActionMove(agentId, MoveDirection.Left);
+                    break;
+                case ConsoleKey.RightArrow:
+                    action = new ActionMove(agentId, MoveDirection.Right);
+                    break;
+                case ConsoleKey.Q:
+                    action = new ActionPickPiece(agentId);
+                    break;
+                case ConsoleKey.W:
+                    action = new ActionCheckPiece(agentId);
+                    break;
+                case ConsoleKey.E:
+                    action = new ActionPutPiece(agentId);
+                    break;
+                case ConsoleKey.R:
+                    action = new ActionDestroyPiece(agentId);
+                    break;
+                case ConsoleKey.A:
+                    action = new ActionCommunicationRequestWithData(agentId, agentId, agentState);
+                    break;
+                case ConsoleKey.S:
+                    action = new ActionCommunicationAgreementWithData(agentId, agentId, random.Next(2) == 1 ? true : false, agentState);
+                    break;
+                case ConsoleKey.D:
+                    action = new ActionDiscovery(agentId);
+                    break;
+                default:
+                    throw new ArgumentException("Invalid input");
+            }
+
+            return action;
         }
     }
 }
