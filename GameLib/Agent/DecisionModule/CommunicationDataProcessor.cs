0d50711a942b78d6d4e3cd0f71e837973e649b01
adamw00000 <adam.wawrzenczyk@gmail.com>
2019-03-20 20:49:34 +0100

    CommunicationDataProcessor handles communication rejection properly

--- GameLib/Agent/DecisionModule/CommunicationDataProcessor.cs
+++ GameLib/Agent/DecisionModule/CommunicationDataProcessor.cs
@@ -8,30 +8,35 @@ namespace GameLib
     {
         public Dictionary<int, (DateTime timestamp, CommunicationData data)> CurrentAgentCommunicationData { get; } =
             new Dictionary<int, (DateTime timestamp, CommunicationData data)>();
+        public Dictionary<int, (DateTime timestamp, CommunicationState state)> LastCommunicationState = 
+            new Dictionary<int, (DateTime timestamp, CommunicationState state)>();
 
-        object Serialize(CommunicationData data)
-        {
-            return data;
-        }
-
-        CommunicationData Deserialize(object data)
+        CommunicationData Unpack(object data)
         {
             return (CommunicationData)data;
         }
 
         public void ExtractCommunicationData(int senderId, bool agreement, DateTime timestamp, object data, AgentState agentState)
         {
+            if (!agreement)
+            {
+                LastCommunicationState[senderId] = (timestamp, CommunicationState.Rejected);
+                return;
+            }
+
+            LastCommunicationState[senderId] = (timestamp, CommunicationState.Accepted);
             (DateTime oldTimestamp, CommunicationData oldData) = CurrentAgentCommunicationData[senderId];
             if (oldTimestamp > timestamp)
                 return;
 
-            CommunicationData deserializedData = Deserialize(data);
+            CommunicationData deserializedData = Unpack(data);
             agentState.UpdateBoardWithCommunicationData(deserializedData.AgentState.Board);
             CurrentAgentCommunicationData[senderId] = (timestamp, deserializedData);
         }
 
-        public object CreateCommunicationData(AgentState state)
+        public object CreateCommunicationDataForCommunicationWith(int targetId, AgentState state)
         {
+            LastCommunicationState[targetId] = (DateTime.UtcNow, CommunicationState.Pending);
             return new CommunicationData(state);
         }
     }
@@ -45,4 +50,6 @@ namespace GameLib
             AgentState = state;
         }
     }
+
+    public enum CommunicationState { Accepted, Rejected, Pending }
 }
