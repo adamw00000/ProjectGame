d1a0714612b854587553ed8cd35b8feed508fbc8
Marcin Zakrzewski <zakrzewskim2@student.mini.pw.edu.pl>
2019-03-31 16:44:52 +0200

    GetMessageId() extracted

--- GameLib/Agent/AgentFactoryWrapper.cs
+++ GameLib/Agent/AgentFactoryWrapper.cs
@@ -14,52 +14,56 @@ namespace GameLib
             this.agentFactory = agentFactory;
             this.id = id;
         }
+        private string GetMessageId()
+        {
+            return Guid.NewGuid().ToString();
+        }
 
         public ActionMessage MoveMessage(MoveDirection direction)
         {
-            string messageId = Guid.NewGuid().ToString();
+            string messageId = GetMessageId();
             return agentFactory.MoveMessage(id, direction, messageId);
         }
 
         public ActionMessage CheckPieceMessage()
         {
-            string messageId = Guid.NewGuid().ToString();
+            string messageId = GetMessageId();
             return agentFactory.CheckPieceMessage(id, messageId);
         }
 
         public ActionMessage CommunicationAgreementMessage(int senderId, bool acceptsCommunication, object data)
         {
-            string messageId = Guid.NewGuid().ToString();
+            string messageId = GetMessageId();
             return agentFactory.CommunicationAgreementMessage(id, senderId, acceptsCommunication, data, messageId);
         }
 
         public ActionMessage CommunicationRequestMessage(int targetId, object data)
         {
-            string messageId = Guid.NewGuid().ToString();
+            string messageId = GetMessageId();
             return agentFactory.CommunicationRequestMessage(id, targetId, data, messageId);
         }
 
         public ActionMessage DestroyMessage()
         {
-            string messageId = Guid.NewGuid().ToString();
+            string messageId = GetMessageId();
             return agentFactory.DestroyMessage(id, messageId);
         }
 
         public ActionMessage DiscoveryMessage()
         {
-            string messageId = Guid.NewGuid().ToString();
+            string messageId = GetMessageId();
             return agentFactory.DiscoveryMessage(id, messageId);
         }
 
         public ActionMessage PickPieceMessage()
         {
-            string messageId = Guid.NewGuid().ToString();
+            string messageId = GetMessageId();
             return agentFactory.PickPieceMessage(id, messageId);
         }
 
         public ActionMessage PutPieceMessage()
         {
-            string messageId = Guid.NewGuid().ToString();
+            string messageId = GetMessageId();
             return agentFactory.PutPieceMessage(id, messageId);
         }
     }
